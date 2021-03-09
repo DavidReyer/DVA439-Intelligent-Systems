aefc9c00d541af49051d60e5b25a37d8d863097d
istdareye <david.reyer@stud.h-da.de>
2021-03-09 15:06:18 +0100

    added a categorizing script for the edu data. Categorized columns are: raisedhands, VisITedResources, AnnouncementsView, Discussion

---
+++ prepare_edu_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+dataset = pd.read_csv('datasets/1/xAPI-Edu-Data.csv')
+
+def map_function(x):
+    if x < 20:
+        return 20
+    if 20 < x < 40:
+        return 40
+    if 40 < x < 60:
+        return 60
+    if 60 < x < 80:
+        return 80
+    return 100
+
+dataset["raisedhands"] = dataset["raisedhands"].apply(map_function)
+dataset["VisITedResources"] = dataset["VisITedResources"].apply(map_function)
+dataset["AnnouncementsView"] = dataset["AnnouncementsView"].apply(map_function)
+dataset["Discussion"] = dataset["Discussion"].apply(map_function)
+
+dataset.to_csv('datasets/1/xAPI-Edu-Data-categorized.csv')
